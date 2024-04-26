b8e9b9b2cd75e6cb4fa8f3bd1e7e455a727f0bf8
Vishwa <viswanathanselvamoorthy@gmail.com>
2024-04-26 13:55:23 +0530

    .

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -70,15 +70,8 @@ const signInFormValidation = (email, password) => {
   const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
     email
   );
-  const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(
-    password
-  );
 
   if (!isEmailValid) return "Email Not Valid";
-  if (!isPasswordValid) return "Password Not Valid";
-
-  if (!isEmailValid && !isPasswordValid)
-    return "Both Email and Password are Not Valid";
 };
 
 export { signUpFormValidation, signInFormValidation };
